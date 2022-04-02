eff8c53b982c69b0c1e040aafbe7ca5fe2d03978
Owais <87132814+captain0owais@users.noreply.github.com>
2022-04-02 15:58:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-
+left_wrist_x = 0;
+right_wrist_x = 0;
+difference = 0;
 
 function setup(){
     video = createCapture(VIDEO);
-    video.size(500 , 400);
-    canvas = createCanvas(605 , 505);
+    video.size(400 , 300);
+    canvas = createCanvas(505 , 405);
 
     poseNet = ml5.poseNet(video , modelLoded);
 
@@ -12,6 +14,9 @@ function setup(){
 
 function draw(){
     background('#c4fffe');
+    textSize(difference);
+    fill("#00ff80");
+    text('HELLO' , right_wrist_x , left_wrist_x);
 }
 
 function modelLoded(){
@@ -21,5 +26,10 @@ function modelLoded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        left_wrist_x = results[0].pose.leftWrist.x;
+        right_wrist_x = results[0].pose.rightWrist.x;
+        console.log("Left Wrist X = " + left_wrist_x);
+        console.log("Right Wrist X = " + right_wrist_x);
+        difference = ceil(left_wrist_x - right_wrist_x);
     }
 }
